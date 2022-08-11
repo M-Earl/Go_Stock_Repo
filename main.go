4396eeacfc70be2015a2091cc5f3499c6398f0a6
Michael Earl <mearl@ou.edu>
2022-08-10 21:48:59 -0400

    Simple request

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main  
 
-import "fmt"
+import ( 
+	"fmt"
+	"net/http"
+	"io/ioutil"
+)
 
 func main() {
-	fmt.Println("Mike Earl")
+	resp, err := http.Get("https://api.polygon.io/v1/open-close/AAPL/2020-10-14?adjusted=true&apiKey=3WhkjwKYZY4V6wK0su0xzcPFSN0FlzvU")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+		} 
+		bodyString := string(bodyBytes)
+		fmt.Println(bodyString)
+	}
 }
