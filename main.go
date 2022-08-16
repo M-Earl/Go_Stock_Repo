eda980dc20b84f2a89693e726a88a7b719a2b8b2
Mike Earl <mearl473@gmail.com>
2022-08-15 20:13:25 -0400

    Added consts for the endpoints

--- main.go
+++ main.go
@@ -6,8 +6,13 @@ import (
 	"io/ioutil"
 )
 
+const aapl_open_close = "https://api.polygon.io/v1/open-close/AAPL/2020-10-14?adjusted=true&apiKey=3WhkjwKYZY4V6wK0su0xzcPFSN0FlzvU"
+const voo_open_close = "https://api.polygon.io/v1/open-close/VOO/2020-10-14?adjusted=true&apiKey=3WhkjwKYZY4V6wK0su0xzcPFSN0FlzvU"
+const msft_open_close = "https://api.polygon.io/v1/open-close/MSFT/2020-10-14?adjusted=true&apiKey=3WhkjwKYZY4V6wK0su0xzcPFSN0FlzvU"
+
+
 func main() {
-	resp, err := http.Get("https://api.polygon.io/v1/open-close/AAPL/2020-10-14?adjusted=true&apiKey=3WhkjwKYZY4V6wK0su0xzcPFSN0FlzvU")
+	resp, err := http.Get(voo_open_close)
 	if err != nil {
 		fmt.Println(err)
 	} else {
