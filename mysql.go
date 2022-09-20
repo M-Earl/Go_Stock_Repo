41345032fb2f8a28bc629ae8b2c7e14f3ee84d2e
Mike Earl <mearl473@gmail.com>
2022-09-19 22:08:04 -0400

    127.0.0.1 changed to 0.0.0.0

--- mysql.go
+++ mysql.go
@@ -2,6 +2,6 @@ package main
 const mysql_username = "root"
 const mysql_password = "Spartan117!"
 const mysql_net = "tcp"
-const mysql_address = "127.0.0.1:3306"
+const mysql_address = "0.0.0.0:3306"
 const mysql_db_name = "example"
 const mysql_conn = mysql_username + ":" + mysql_password + "@" + mysql_net + "(" + mysql_address + ")/" + mysql_db_name
