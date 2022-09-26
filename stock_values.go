f92fc5dcf3a4f7a48745febbc14b94e29c255ba0
Mike Earl <mearl473@gmail.com>
2022-09-25 22:28:49 -0400

    Added a file relating to db

---
+++ stock_values.go
@@ -0,0 +1,8 @@
+// CREATE TABLE stock_values
+// (
+//   id              INT unsigned NOT NULL AUTO_INCREMENT,
+//   stock           VARCHAR(150) NOT NULL,
+//   date            DATE NOT NULL,
+//   value           INT unsigned NOT NULL,
+//   PRIMARY KEY     (id)
+// );
